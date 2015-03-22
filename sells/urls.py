1682c5bfbae6ec982f90e05525bd5888224ce050
cyberzhg <cyberzhg@gmail.com>
2015-03-22 19:43:39 +0800

    DELETE /sell/post/<post_id>/ #20
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- sells/urls.py
+++ sells/urls.py
@@ -3,6 +3,6 @@ import views
 
 urlpatterns = patterns(
     '',
-    url(r'^post/(?P<post_sell_id>\d+)/$', views.view_update_post),
+    url(r'^post/(?P<post_sell_id>\d+)/$', views.view_post),
     url(r'^post/$', views.view_new_post),
 )
--- sells/views.py
+++ sells/views.py
@@ -23,8 +23,16 @@ def view_new_post(request):
     return HttpResponse(json.dumps({'success': True, 'id': new_post.id}))
 
 
-@request_filter(['PUT'])
-@request_parameter(['username'])
+@request_filter(['GET', 'PUT', 'DELETE'])
+def view_post(request, post_sell_id):
+    # if request.method == 'GET':
+    #     return view_get_post(request, post_sell_id)
+    if request.method == 'PUT':
+        return view_update_post(request, post_sell_id)
+    if request.method == 'DELETE':
+        return view_delete_post(request, post_sell_id)
+
+
 @request_login
 @allow_empty(['title', 'description', 'images'])
 def view_update_post(request, post_sell_id):
@@ -41,4 +49,16 @@ def view_update_post(request, post_sell_id):
         image_id_list = request.data['images'].split(',')
     update_image_set(post.image_set.id, image_id_list)
     post.save()
+    return HttpResponse(json.dumps({'success': True}))
+
+
+@request_login
+def view_delete_post(request, post_sell_id):
+    post = PostSell.objects.filter(id=int(post_sell_id))
+    if post.count() != 1:
+        return errno.response_with_erron(errno.ERRNO_NOT_EXIST)
+    post = post[0]
+    if post.user.id != request.user.id:
+        return errno.response_with_erron(errno.ERRNO_NOT_OWNER)
+    post.delete()
     return HttpResponse(json.dumps({'success': True}))
